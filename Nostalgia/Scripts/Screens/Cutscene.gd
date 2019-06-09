2daf507fa1d681a2afd6b1443d383a2d1f8cde66
fiwon123 <felipeinoue@yahoo.com.br>
2019-06-09 02:33:32 -0300

    Add sounds

--- Nostalgia/Scripts/Screens/Cutscene.gd
+++ Nostalgia/Scripts/Screens/Cutscene.gd
@@ -3,9 +3,9 @@ extends Node2D
 onready var audio_button_skip = $ColorRect/ButtonSkip/AudioButtonSkip
 
 func _ready():
-	$AnimatedSprite.play("default")
+	$AnimationPlayer.play("default")
 	Global.audio.volume_db = -4000
 
 
-func _on_AnimatedSprite_animation_finished():
+func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().change_scene(Global.LEVEL1)
